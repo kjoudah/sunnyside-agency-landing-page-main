a8717e776b7b1d1f85c395e8bcc809ea49fce7d0
Karam <karamjoudah@hotmail.com>
2023-05-15 22:50:44 -0400

    finished up theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Barlow', 'system-ui'],
+      serif: ['Fraunces', 'system-ui'],
+    },
+    fontWeight: {
+      semibold: 600,
+      bold: 700,
+      black: 900,
+    },
+    extend: {
+      colors: {
+        'red-200': 'hsl(var(--red-200))',
+        'yellow-200': 'hsl(var(--yellow-200))',
+        'cyan-200': 'hsl(var(--cyan-200))',
+        'blue-400': 'hsl(var(--blue-400))',
+        'cyan-400': 'hsl(var(--cyan-400))',
+        'blue-600': 'hsl(var(--blue-600))',
+        'blue-800': 'hsl(var(--blue-800))',
+        'blue-700': 'hsl(var(--blue-700))',
+        'blue-500': 'hsl(var(--red-blue-500))',
+      },
+    },
   },
   plugins: [],
 };
